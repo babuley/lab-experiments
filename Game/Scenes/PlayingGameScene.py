41feff9786163055b07a14391ef5468ccf2aac40
aszczepaniak <adam.szczepaniak@allocatesoftware.com>
2020-01-03 10:13:10 +0000

    More functional
    Signed-off-by: aszczepaniak <adam.szczepaniak@allocatesoftware.com>

--- Game/Scenes/PlayingGameScene.py
+++ Game/Scenes/PlayingGameScene.py
@@ -6,41 +6,37 @@ from Shared import Boulder
 
 import random
 
+
 class PlayingGameScene(Scene):
 
     def __resetBlackBox(self):
         self.__blackBox = []
 
-    def __init__(self, game):         
+    def __init__(self, game):
         super(PlayingGameScene, self).__init__(game)
         self.__resetBlackBox()
 
     def render(self):
         super(PlayingGameScene, self).render()
- 
-        player = self.getGame().getPlayer()   
-        player.render() 
+
+        player = self.getGame().getPlayer()
+        player.render()
 
         self.clearText()
 
-        self.addText("Sokoban " ,
-                     x = 0,
-                     y = GameConstants.SCREEN_SIZE[1] - 60, size = 30)
+        self.addText("Sokoban ",
+                     x=0,
+                     y=GameConstants.SCREEN_SIZE[1] - 60, size=30)
 
-
-        self.addText("Level: " + str(self.getGame().getLevel()) ,
-                     x = 0,
-                     y = GameConstants.SCREEN_SIZE[1] - 30, size = 30)
+        self.addText("Level: " + str(self.getGame().getLevel()),
+                     x=0,
+                     y=GameConstants.SCREEN_SIZE[1] - 30, size=30)
 
     def isPositionOccupied(self, pos):
-        walls = self.getGame().getAssets()
-        occupied = False
-        for w in walls:
-            if w.getPosition()[0] == pos[0] and w.getPosition()[1] == pos[1]:
-                occupied = True
-                break           
-        b = self.getBoulderAtPosition(pos)               
-        return occupied or b 
+        def taken(w): return w.getPosition()[
+            0] == pos[0] and w.getPosition()[1] == pos[1]
+        occupied = len(list(filter(taken, self.getGame().getAssets()))) != 0
+        return occupied or len(self.getBoulderAtPosition(pos)) != 0
 
     def isSceneCompleted(self):
         boulders = self.getGame().getBoulders()
@@ -49,70 +45,62 @@ class PlayingGameScene(Scene):
         for b in boulders:
             for t in targets:
                 if b.getPosition()[0] == t.getPosition()[0] and b.getPosition()[1] == t.getPosition()[1]:
-                    c+=1
-        return c == len(boulders)           
-
+                    c += 1
+        return c == len(boulders)
 
     def getBoulderAtPosition(self, pos):
-        boulders = self.getGame().getBoulders()
-        for b in boulders:
-            if b.getPosition()[0] == pos[0] and b.getPosition()[1] == pos[1]:
-                return b
-        return None       
+        def cond(b): return b.getPosition()[
+            0] == pos[0] and b.getPosition()[1] == pos[1]
+        return list(filter(cond, self.getGame().getBoulders()))
 
     def newPositionRight(pos):
-        return (pos[0] +  GameConstants.TILE_SIZE[0] , pos[1] )
+        return (pos[0] + GameConstants.TILE_SIZE[0], pos[1])
 
     def newPositionLeft(pos):
-        return (pos[0] -  GameConstants.TILE_SIZE[0] , pos[1] )  
+        return (pos[0] - GameConstants.TILE_SIZE[0], pos[1])
 
     def newPositionUp(pos):
-        return (pos[0] , pos[1] - GameConstants.TILE_SIZE[0] )  
+        return (pos[0], pos[1] - GameConstants.TILE_SIZE[0])
 
     def newPositionDown(pos):
-        return (pos[0] , pos[1] + GameConstants.TILE_SIZE[0] )  
+        return (pos[0], pos[1] + GameConstants.TILE_SIZE[0])
 
-
-    __directions = {"RIGHT":  newPositionRight, "LEFT" : newPositionLeft, 
-    "UP" :newPositionUp, "DOWN": newPositionDown    }
+    __directions = {"RIGHT":  newPositionRight, "LEFT": newPositionLeft,
+                    "UP": newPositionUp, "DOWN": newPositionDown}
 
     def findBoulder(self, cand):
-        for r in self.getGame().getBoulders():
-            if r.getId() == cand.getId():
-                return r
-        return None
+        return list(filter(lambda r: r.getId() == cand.getId(), self.getGame().getBoulders()))
 
     def moveBack(self):
         if len(self.__blackBox) > 0:
             o = self.__blackBox.pop()
-            player = self.getGame().getPlayer()  
+            player = self.getGame().getPlayer()
             player.setPosition(o[0].getPosition())
             if o[1]:
                 b = self.findBoulder(o[1])
-                if b:
-                    b.setPosition(o[1].getPosition())
+                if len(b) != 0:
+                    b[0].setPosition(o[1].getPosition())
 
-  
     def moveTo(self, dir):
-        player = self.getGame().getPlayer()    
-        c = player.getPosition() 
+        player = self.getGame().getPlayer()
+        c = player.getPosition()
         newPosition = self.__directions[dir](c)
-        b = self.getBoulderAtPosition(newPosition)  
+        b = self.getBoulderAtPosition(newPosition)
         bb = Boulder(newPosition, None, None)
         p = Player(c, None, None)
-        
-        if b:
-            bb = Boulder(newPosition, None, None)
-            bb.setId(b.getId())
-        self.__blackBox.append([p,bb])                               
 
-        if b:
+        if len(b) != 0:
+            bb = Boulder(newPosition, None, None)
+            bb.setId(b[0].getId())
+        self.__blackBox.append([p, bb])
+
+        if len(b) != 0:
             boulderNewPosition = self.__directions[dir](newPosition)
             if not self.isPositionOccupied(boulderNewPosition):
-                b.setPosition(boulderNewPosition)                                  
+                b[0].setPosition(boulderNewPosition)
         o = self.isPositionOccupied(newPosition)
-        if not o:                                           
-            player.setPosition( newPosition )        
+        if not o:
+            player.setPosition(newPosition)
             r = random.randint(0, 13)
             if r % 5 == 0:
                 self.getGame().playSound()
@@ -121,26 +109,25 @@ class PlayingGameScene(Scene):
 
     def onGameComplete(self):
         self.__resetBlackBox()
-        self.getGame().goToNextLevel()    
+        self.getGame().goToNextLevel()
 
     def handleEvents(self, events):
         super(PlayingGameScene, self).handleEvents(events)
         for event in events:
             if event.type == pygame.QUIT:
                 exit()
-            
-            if event.type == pygame.KEYDOWN:            
-                if event.key == pygame.K_BACKSPACE:  
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
                     self.moveBack()
-                if event.key == pygame.K_RIGHT:                    
+                if event.key == pygame.K_RIGHT:
                     self.moveTo("RIGHT")
-                if event.key == pygame.K_LEFT:  
-                    self.moveTo("LEFT")                                       
-                if event.key == pygame.K_UP:                    
-                    self.moveTo("UP")                                     
-                if event.key == pygame.K_DOWN:                    
-                    self.moveTo("DOWN")         
+                if event.key == pygame.K_LEFT:
+                    self.moveTo("LEFT")
+                if event.key == pygame.K_UP:
+                    self.moveTo("UP")
+                if event.key == pygame.K_DOWN:
+                    self.moveTo("DOWN")
 
                 if self.isSceneCompleted():
-                    self.onGameComplete()                        
- 
+                    self.onGameComplete()
--- beadspuzzle/beads.py
+++ beadspuzzle/beads.py
@@ -1,74 +1,77 @@
 import random
 import math
+from functools import reduce
 
-#Symetrical side
+# Symetrical side
 totalBeads = 8
-symbols = ['O','X']
+symbols = ['O', 'X']
 
-#Randomly generate candidates of arrays of particular combination size
-def getMeCandidateArray(sizeOfTarget):    
+# Randomly generate candidates of arrays of particular combination size
+
+
+def getMeCandidateArray(sizeOfTarget):
     cand = []
     for i in range(sizeOfTarget):
-        o = random.randint(0,1)        
+        o = random.randint(0, 1)
         cand.append(o)
-    return cand        
+    return cand
 
-#Check whether 2 arrays are the same
+# Check whether 2 arrays are the same
+
+
 def isTheSameArray(a, b):
     c = 0
     for i in range(len(a)):
         for j in range(len(b)):
             if i == j and a[i] == b[j]:
-                c = c +1
+                c = c + 1
     return c == len(a)
 
-#Check whether we already have the given combination in the stash
+# Check whether we already have the given combination in the stash
+
+
 def alreadyIn(all, cand):
     for alreadyIn in all:
         if isTheSameArray(alreadyIn, cand):
             return True
     return False
 
-#Calculate the sum of the elements in the array
-def getSum(r):    
-    s = 0
-    for c in r:
-        s+=c
-    return s
+# Calculate the sum of the elements in the array
 
-#Just to decorate 0,1 to nicer symbols. like O and X, etc, or A,B or + -
+
+def getSum(r):
+    return reduce(lambda x, y: (x + y), r)
+
+# Just to decorate 0,1 to nicer symbols. like O and X, etc, or A,B or + -
+
+
 def decorateSymbols(r):
-    result = []
-    for f in r:
-        result.append(symbols[f])
-    return result
+    return list(map(lambda a: symbols[a]))
 
-#That is benomial coefficient
+
 def getTarget(b):
-    return math.factorial(b) / ( (math.factorial(b/2) * math.factorial(b - b/2)) )
+    return math.factorial(b) / ((math.factorial(b/2) * math.factorial(b - b/2)))
 
-#Find all the combinations that meet the symmetry condition 
+# Find all the combinations that meet the symmetry condition
+
+
 def getCombinations(totalBeads):
     beadsOnSide = int(totalBeads / 2)
     targetSet = []
     while len(targetSet) < getTarget(beadsOnSide):
-        r = getMeCandidateArray(beadsOnSide)                
-        if (not alreadyIn(targetSet,r) and getSum(r) == beadsOnSide/2):            
+        r = getMeCandidateArray(beadsOnSide)
+        if (not alreadyIn(targetSet, r) and getSum(r) == beadsOnSide/2):
             targetSet.append(r)
     return targetSet
 
-#Decorate the final result
-def decorateArray(cand):    
-    outer = []
-    for i in cand:
-        inner = []
-        for j in i:
-            inner.append(symbols[j])
-        outer.append(inner)
-    return outer
+# Decorate the final result
+
+
+def decorateArray(cand):
+    return list(map(lambda i: list(map(lambda a: symbols[a], i)), cand))
 
 
 print("Number of total beads:", totalBeads)
 print("Number of beads on side if symmetrical:", int(totalBeads / 2))
-print("Target number of combinations:", getTarget(int(totalBeads/ 2)))
-print(decorateArray(getCombinations(totalBeads)))        
+print("Target number of combinations:", getTarget(int(totalBeads / 2)))
+print(decorateArray(getCombinations(totalBeads)))
